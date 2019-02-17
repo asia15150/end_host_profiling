51dda8e482877d1deef19981ac66c538833b2f3b
haneul <kehoecicco@live.fr>
2019-02-17 13:23:56 +0100

    hello

--- main.py
+++ main.py
@@ -12,6 +12,22 @@ import csv
 import matplotlib.pyplot as plt
 #from random import choice
 import numpy as np
+from sklearn import svm
+from sklearn.svm import SVC
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import itertools
+from mpl_toolkits import mplot3d
+
+
+
+
+
 #import pprint
 
 #font = {'size'   : 30}
@@ -46,14 +62,16 @@ class Graphlet:
         row[2] = 'dstIP:'+row[2]
         row[3] = 'sPort:'+row[3]
         row[4] = 'dPort:'+row[4]
-        row[5] = 'anomalies:'+row[5]
-        
+        #row[5] = 'anomalies:'+row[5]
+        self.anomalie = row[5]
         #self.srcIp.append(row[0])
         self.protocol.add(row[1])
         self.dstIp.add(row[2])
         self.sPort.add(row[3])
         self.dPort.add(row[4])
-        self.anomalies.add(row[5])
+        #self.anomalies.add(row[5])
+        row = row[:-1]
+        #print(row)
         self.tab_nodes = self.tab_nodes.union(self.makeNodes(row))
 
     #build the matrix to do the random walk
@@ -82,20 +100,23 @@ class Graphlet:
         G=nx.from_pandas_edgelist(df, 'from', 'to')
         nx.draw(G, with_labels=True)
 
-
-    # draw version 1
-    def draw(self):
-        #nodes
+    def make_graph(self):
+         #nodes
         self.graph.add_node(self.ip_adress, label = 'srcIp')
         self.graph.add_nodes_from(list(self.protocol), label = 'protocol')
         self.graph.add_nodes_from(list(self.dstIp), label = 'dstIP')
         self.graph.add_nodes_from(list(self.sPort), label = 'sPort')
         self.graph.add_nodes_from(list(self.dPort), label = 'dPort')
-        self.graph.add_nodes_from(list(self.anomalies), label = 'anomalies')
+        #self.graph.add_nodes_from(list(self.anomalies), label = 'anomalies')
 
         #edges
         self.graph.add_edges_from(self.tab_nodes)
         
+
+    def get_labels_size(self):
+        return len(self.graph)
+    # draw version 1
+    def draw(self):  
         #graphlet1.nodes(data=True)
         
         color_map = []
@@ -137,7 +158,7 @@ def readAnnotatedTrace():
             else:
                G.saveRowInArrays(row)
                
-            #if line_count < 1000:#####CHANGE TO 10000 TO SEE DIFFERENC####FOR NOW I ONLY WORK WITH srcIp=882
+            #if line_count < 100:#####CHANGE TO 10000 TO SEE DIFFERENC####FOR NOW I ONLY WORK WITH srcIp=882
                #line_count+=1
             #else:
                #break
@@ -145,43 +166,138 @@ def readAnnotatedTrace():
     return graphlets_
 
 
-def compute_walk(length, matrix):
+def compute_walk(length, matrix, size):
     A = matrix
+    I = np.zeros((size, size), int)
+    np.fill_diagonal(I, 1)
+    #print(A)
+    #print(I)
+    I = np.add(I, A)
     for i in range(length-1):
         #print("####walk: "+str(i+1))
-        #print(A)
+        #print(A)       
         A = np.matmul(A, matrix)
+        #print(A)
+        I = np.add(I, A)
     #print("####walk: 4")
     #print(A)
-    return A
+    return [A, I]
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+def plot_svm(X, y):
+    h = .02  # step size in the mesh
+
+    # we create an instance of SVM and fit out data. We do not scale our
+    # data since we want to plot the support vectors
+    C = 1.0  # SVM regularization parameter
+    svc = svm.SVC(kernel='linear', C=C).fit(X, y)
+    rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, y)
+    poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
+    lin_svc = svm.LinearSVC(C=C).fit(X, y)
+
+    # create a mesh to plot in
+    #print (X)
+    #print (X[:, 0].max() + 1)
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),np.arange(y_min, y_max, h))
+
+    # title for the plots
+    titles = ['SVC with linear kernel',
+              'LinearSVC (linear kernel)',
+              'SVC with RBF kernel',
+              'SVC with polynomial (degree 3) kernel']
 
 
+    for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+        plt.subplot(2, 2, i + 1)
+        plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+
+    # Plot also the training points
+        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+        plt.xlabel('Sepal length')
+        plt.ylabel('Sepal width')
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.xticks(())
+        plt.yticks(())
+        plt.title(titles[i])
+    plt.show()
+
+
+    
 graphlets_ = readAnnotatedTrace()
+features = {}
+array_matrix = []
+array_labels = []
+size_max = 0
 for index, g in enumerate(graphlets_.values()):
-   
-    i = 200+index+1
+    g.make_graph()
+    g.make_first_matrix()
+    #i = 200+index+1
     #print(i)
     #plt.subplot(i)
     
-    fig = plt.figure(figsize=(20,20))
-    plt.subplot(2, 1, 1)
-    g.draw()
+    #fig = plt.figure(figsize=(20,20))
+    #plt.subplot(2, 1, 1)
+    #g.draw()
 
-    plt.subplot(2, 1, 2)
-    g.make_first_matrix()
-    B = compute_walk(4, g.get_first_matrix())
-    df = pd.DataFrame(B, columns=g.graph.nodes(), index=g.graph.nodes())
+    #plt.subplot(2, 1, 2)
+    #g.make_first_matrix()
+    
+    #B = compute_walk(4, g.get_first_matrix(), size)
+    #df = pd.DataFrame(B, columns=g.graph.nodes(), index=g.graph.nodes())
     #df[0].plot()
-    A = np.squeeze(np.asarray(B))
-    plt.table(cellText=df.values,
-    rowLabels=df.index, colLabels=df.columns,
-    loc='center', fontsize=30)
-    plt.axis('off')
+    #A = np.squeeze(np.asarray(B))
+    #plt.table(cellText=df.values,
+    #rowLabels=df.index, colLabels=df.columns,
+    #loc='center', fontsize=30)
+    #plt.axis('off')
     #df.plot()
     
-    fig.savefig('plots/'+g.ip_adress+'.png', dpi=200)
-    plt.close(fig) 
+    #fig.savefig('plots/'+g.ip_adress+'.png', dpi=200)
+    #plt.close(fig) 
     
     
     #B = compute_walk(4, g.get_first_matrix())
@@ -190,8 +306,47 @@ for index, g in enumerate(graphlets_.values()):
     #print(B)
     #print(df)
     #plt.figure(index)
+    size = g.get_labels_size()
+    if size > size_max:
+        size_max = sizesize = g.get_labels_size()
+    matrix = compute_walk(4, g.get_first_matrix(), size)
+    array_matrix.append(matrix[1])
+    if "normal" in g.anomalie:
+        array_labels.append("normal")
+    else:
+        array_labels.append("anomalie")
+    #features.update({g.ip_adress: (np.count_nonzero(matrix[1]),g.anomalie)})
+
+array = []
+for m in array_matrix:
+
+    a = np.array(m)
+    a = np.resize(a,(size_max,size_max))
+    a = np.squeeze(a)
+    a = a.flatten()
+    array.append(a)
+
+array_labels = np.asarray(array_labels)
+
+
+X_train, X_test, y_train, y_test  =  train_test_split(array, array_labels, test_size=1.0, random_state=54, shuffle=False)
+#print(c)
+
+X = array
+y = array_labels
+
+
+clf =  svm.SVC(kernel='poly', degree=size_max ,gamma='auto')
+clf.fit(array, array_labels)
+
+
+y_pred = clf.predict(X_test)
+classes = [ "anomalie", "normal"]
+print(accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred, target_names=classes))
+cnf_matrix = confusion_matrix(y_test, y_pred)
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=classes,title='Confusion matrix, without normalization')
 
 #plt.show()
-
-
-
+plt.show()
