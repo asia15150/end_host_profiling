cf86a4a3abcf7fb9028802e415af9643cbab558f
Joanna Swiderska <joanna.swiderskasia@gmail.com>
2019-03-01 12:01:00 +0100

    cleaning up final file / dividing into 3 files

--- final.py
+++ final.py
@@ -11,8 +11,6 @@ from multiprocessing import Pool
 import pandas as pd
 import networkx as nx
 import csv
-import matplotlib.pyplot as plt
-#from random import choice
 import numpy as np
 
 from sklearn import svm
@@ -135,6 +133,13 @@ class Graphlet:
                 color_map.append('pink')
         nx.draw_shell(self.graph, with_labels=True,node_color = color_map, node_size=500)
 
+
+
+
+
+
+
+
         
 #read file and build graphlets
 def readTrace(file):
@@ -183,108 +188,6 @@ def compute_walk(length, matrix, size):#compute walk
 
 
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
-
-def damping_factor(direct_product_matrix):
-    #print(direct_product_matrix)
-    maxInDegree = 0
-    maxOutDegree = 0
-    sum_all_columns = direct_product_matrix.sum(axis=0)
-    #print(sum_all_columns[0,0])
-    sum_all_row = direct_product_matrix.sum(axis=1)
-    #print(sum_all_row)
-    l = len(direct_product_matrix)
-    for index in range(l):
-        maxInDegree = max(maxInDegree, sum_all_columns[index])
-        #print(maxInDegree)
-        maxOutDegree = max(maxOutDegree, sum_all_row[index])
-        #print(maxOutDegree)
-    #print(maxInDegree)
-    #print(maxOutDegree)
-    
-    factor = 1/min(maxInDegree,maxOutDegree)
-    return factor
-
-
-
-
-def direct_product_kernel(matrix_adj_A, matrix_adj_B):
-    l_A = len(matrix_adj_A)#size of matrix adjacency A
-    l_B = len(matrix_adj_B)#size of matrix adjacency B
-
-    #print(np.array(matrix_adj_A))
-    #print(np.array(matrix_adj_B))
-    products = None
-
-    ######the result of [A, A] direct product [A, A] is
-    #[AA, AA, AA, AA]
-    for i in range(l_A):#for each column of A
-        matrix = None
-        for j in range(l_A):#for each row of A
-            #multiply an element of A with the whole matrix of B
-            new_matrix = matrix_adj_A[i,j] * matrix_adj_B #multiply an element of 
-
-            #we concatenate the matrix
-            if j == 0:# we initialise new line with the first chunk
-                matrix = new_matrix
-            else:
-                matrix = np.concatenate((matrix, new_matrix), axis=1)
-                #we concatenate the next chunk to the same line
-        if i == 0:#concatenate by column
-            products = matrix
-            #print(products)
-        else:
-            products = np.concatenate((products, matrix), axis=0)
-            #print(matrix)
-    #print("\n\n\nDIRECT PRODUCT MATRIX \n\n",matrix)
-
-
-    #we now can compute the goemetric sum
-    #(I - dpf*direct_product_matrix)exposant(-1))
-    dpf = damping_factor(products)
-    #print("\n\nDAMPING FACTOR : ", dpf,"\n\n")
-    
-    I = np.identity(len(products))#matrix identity
-    
-    k = np.linalg.inv(np.subtract(I, dpf*products))#inversion
-    return np.array(k).sum()
-
-
-            
-            
-
 
 def reshape_matrix(array_matrix, size_max):
     array = []
@@ -299,21 +202,6 @@ def reshape_matrix(array_matrix, size_max):
         array.append(a)
     return array
 
-def draw(g):
-    fig = plt.figure(figsize=(20,20))
-    plt.subplot(2, 1, 1)
-    g.draw()
-    plt.subplot(2, 1, 2)
-    return fig
-
-def draw2(fig, g):
-    df = pd.DataFrame(g, columns=g.graph.nodes(), index=g.graph.nodes())
-    #A = np.squeeze(np.asarray(B))
-    plt.table(cellText=df.values,
-                rowLabels=df.index, colLabels=df.columns,
-                loc='center', fontsize=30)
-    plt.axis('off')
-    fig.savefig('plots/'+g.ip_adress+'.png', dpi=200)
 
 
 ####à faire!!!!!!!!!donne les matrix non annotated dans cette fonction et
@@ -433,113 +321,6 @@ def main_random_walk():
     classification_annotated_rbf(array, array_labels, size_max, array_not)
 #classification_annotated_linear(array, array_labels, size_max, array_not)
 
-def main_direct_product():
-    adjacencies = []
-    anomalie = None
-    normal = None
-    anomalies = []
-    for index, g in enumerate(graphlets_.values()):
-        g.make_graph()
-        g.make_first_matrix()
-        adjacencies.append(np.array(g.get_first_matrix()))
-        if(g.anomalie != -1):
-            if "normal" in g.anomalie:
-                normal = np.array(g.get_first_matrix())       
-            else:
-                print(g.ip_adress)
-                print(index)
-                anomalie = np.array(g.get_first_matrix())
-                anomalies.append(anomalie)
-        
-    #print(adjacencies)
-    #dpk = direct_product_kernel(adjacencies[0],adjacencies[1])
-    #print(normal.shape)
-    #print(anomalie.shape)
-    #dpk = direct_product_kernel(normal,anomalie)
-
-
-    #list = np.zeros((1,200))
-    #for i in range(1):
-     #   for j in range(200):
-    #        list[i,j] = (int(direct_product_kernel(adjacencies[14],adjacencies[j])))
-    #print(list)
-
-    #direct = direct_product_kernel_v2(adjacencies[14],adjacencies[j])
-    #print(direct)
-    
-    #dpk = direct_product_kernel(adjacencies[10],adjacencies[100])
-    #print(list)
-    #print("RESULT AFTER SUMMING UP : ",dpk,"\n\n")
-    #l = len(adjacencies)
-    #kernel_matrix = np.zeros((l,l))
-    #print(kernel_matrix.shape)
-    #time_start = time.process_time()
-    #run your code
-
-
-graphlets_ = readTrace('annotated-trace.csv')
-graphlets_not = readTrace('not-annotated-trace.csv')
-
-
-def damping_factor_v2(direct_product_matrix):
-    maxInDegree = np.amax(direct_product_matrix.sum(axis=0))#somme columns then find max 
-    maxOutDegree = np.amax(direct_product_matrix.sum(axis=1))#somme row then find max 
-    factor = 1/min(maxInDegree,maxOutDegree)
-
-    #print('DAMPING-FACTOR--------',factor,'---------------')
-    return factor
-    
-def direct_product_kernel_v2(matrix_adj_A, matrix_adj_B):
-    lA = len(matrix_adj_A)
-    lB = len(matrix_adj_B)
-    size = lA*lB
-    direct_product = np.einsum('ij,kl->ikjl',matrix_adj_A,matrix_adj_B).reshape(size,size)#direct product or tendordot then reshape
-    #print(direct_product)
-    dpf = damping_factor_v2(direct_product)
-
-    I = np.identity(size)#matrix identity
-    
-    k = np.linalg.inv(np.subtract(I, dpf*direct_product)).sum()#inversion
-    #print('Direct Product Sum Kernel Value--------',k,'---------------')
-    return k
-
-def main_direct_product_v2():
-    adjacencies = []
-    anomalie = None
-    normal = None
-    anomalies = []
-    for index, g in enumerate(graphlets_.values()):
-        g.make_graph()
-        g.make_first_matrix()
-        adjacencies.append(np.array(g.get_first_matrix()))
-        if(g.anomalie != -1):
-            if "normal" in g.anomalie:
-                normal = np.array(g.get_first_matrix())       
-            else:
-                #print(g.ip_adress)
-                #print(index)
-                anomalie = np.array(g.get_first_matrix())
-                anomalies.append(anomalie)
-    list_ = np.zeros((100,1001))
-
-    
-
-    #time_start = time.process_time()
-    #for i in range(2):
-        #Parallel(n_jobs=2)(delayed(direct_product_kernel_v2)(adjacencies[i],adjacencies[j]) for j in range(1001))
-    #time_end = time.process_time()
-    #print(list_)
-    #print('time::::',time_end-time_start)
-
-    
-
-
-
-    
-main_direct_product_v2()
-
-    
-#main_direct_product()
 #main_random_walk()
 
 
